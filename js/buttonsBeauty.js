6f76ddca3be0db1b37dda2a75b64b9e7cdb50f6b
zornitsawolf@gmail.com <zornitsawolf@gmail.com?>
2021-07-25 21:54:47 +0300

    Fixes

---
+++ js/buttonsBeauty.js
@@ -0,0 +1,6 @@
+function changeMeGeneral() {
+  var element = document.getElementById("general_btn");
+  element.classList.toggle("has-flag");
+  var elementUno = document.getElementById("general_modal");
+  elementUno.classList.toggle("has-flag");
+}
